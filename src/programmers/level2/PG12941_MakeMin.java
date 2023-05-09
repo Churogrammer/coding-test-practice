3bd4da439acdbb0fb5649296513cf696494a2328
churogrammer <m_sense1031@naver.com>
2023-05-09 23:15:24 +0900

    add : PG Level2

--- src/programmers/level2/PG12941_MakeMin.java
+++ src/programmers/level2/PG12941_MakeMin.java
@@ -1,2 +1,23 @@
-package programmers.level2;public class PG12941_MakeMin {
+package programmers.level2;
+
+import java.util.Arrays;
+
+/*
+* 최솟값 만둘기
+* https://school.programmers.co.kr/learn/courses/30/lessons/12941
+* */
+public class PG12941_MakeMin {
+    public int solution(int []A, int []B)
+    {
+        int answer = 0;
+
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        for(int i = 0; i < A.length;i++){
+            answer += (A[i] * B[A.length - i-1]);
+        }
+
+        return answer;
+    }
 }
--- src/programmers/level2/PG70124_Repeat_BinaryChange.java
+++ src/programmers/level2/PG70124_Repeat_BinaryChange.java
@@ -1,2 +1,23 @@
-package programmers.level2;public class PG70124_Repeat_BinaryChange {
+package programmers.level2;
+
+/*
+* 이진 변환 반복하기
+* https://school.programmers.co.kr/learn/courses/30/lessons/70129
+* */
+public class PG70124_Repeat_BinaryChange {
+    public int[] solution(String s) {
+        int[] answer = new int[2];
+
+        while(s.length() > 1){
+            answer[0]++;
+
+            int length = s.length();
+            s = s.replace("0", "");
+            answer[1] += length - s.length();
+
+            s = Integer.toBinaryString(s.length());
+        }
+
+        return answer;
+    }
 }
