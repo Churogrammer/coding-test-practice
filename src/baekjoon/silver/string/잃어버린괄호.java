0f627eed17e2ee49a81fec3ed0ed6de185e3d789
churogrammer <m_sense1031@naver.com>
2023-05-13 23:36:02 +0900

    mod : 주석 코드 제거

--- src/baekjoon/silver/string/잃어버린괄호.java
+++ src/baekjoon/silver/string/잃어버린괄호.java
@@ -3,7 +3,6 @@ package baekjoon.silver.string;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-import java.util.TreeMap;
 
 public class 잃어버린괄호 {
     public static void main(String[] args) {
@@ -12,10 +11,8 @@ public class 잃어버린괄호 {
         List<Integer> list = new ArrayList<>();
 
         for (String s : str.split("-")) {
-            //System.out.println(s);
             int val = 0;
             for (String num : s.split("\\+")) {
-                //System.out.println(num);
                 val += Integer.parseInt(num);
             }
             list.add(val);
@@ -26,27 +23,5 @@ public class 잃어버린괄호 {
             ans -= a;
         }
         System.out.println(ans);
-        TreeMap treeMap = new TreeMap();
     }
-//
-//
-//
-//        for(char c : str.toCharArray()){
-//            if(c == '+' || c == '-'){
-//                list.add(Integer.parseInt(sb.toString()));
-//                opList.add(c);
-//                sb = new StringBuilder();
-//                continue;
-//            }
-//            sb.append(c);
-//        }
-//
-//        int ans = opList.get(0);
-//        for(Character op : opList){
-//            if(op == '-'){
-//
-//            }
-//        }
-//
-//    }
 }
